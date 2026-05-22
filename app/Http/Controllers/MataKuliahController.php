8ee535fd44d30fafd3eee59d0d7b77d538b4d879
Arnol <andikamaster13@gmail.com>
2026-05-22 15:57:43 +0700

    Mata Kuliah Update model

--- app/Http/Controllers/MataKuliahController.php
+++ app/Http/Controllers/MataKuliahController.php
@@ -7,59 +7,42 @@ use Illuminate\Http\Request;
 
 class MataKuliahController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+
+        $daftarMataKuliah = MataKuliah::all();
+
+
+        return view('matakuliah.index', compact('daftarMataKuliah'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('matakuliah.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(MataKuliah $mataKuliah)
-    {
-        //
-    }
+        $request->validate([
+            'kodeMK'   => 'required|string|unique:mata_kuliah,kodeMK',
+            'namaMK'   => 'required|string|min:3',
+            'sks'      => 'required|integer|min:1|max:6',
+            'semester' => 'required|integer|between:1,8',
+        ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(MataKuliah $mataKuliah)
-    {
-        //
-    }
+        MataKuliah::create([
+            'kodeMK'   => $request->kodeMK,
+            'namaMK'   => $request->namaMK,
+            'sks'      => $request->sks,
+            'semester' => $request->semester,
+        ]);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, MataKuliah $mataKuliah)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(MataKuliah $mataKuliah)
-    {
-        //
+        return redirect()->route('matakuliah.index')->with('sukses', 'Mata Kuliah berhasil ditambahkan!');
     }
 }
+
+
+
--- app/Models/MataKuliah.php
+++ app/Models/MataKuliah.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class MataKuliah extends Model
 {
-    //
+    protected $table = 'mata_kuliah';
+
+    protected $primaryKey = 'kodeMK';
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'kodeMK',
+        'namaMK',
+        'sks',
+        'semester'
+    ];
+
+    public function getKodeMK(): string
+    {
+        return $this->kodeMK;
+    }
+
+    public function getSKS(): int
+    {
+        return $this->sks;
+    }
+
+    public function tampilkanInfoMK(): string
+    {
+        return "[{$this->kodeMK}] {$this->namaMK} - {$this->sks} SKS (Semester {$this->semester})";
+    }
 }
