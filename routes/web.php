de47d5dd6c6b06a431e546633f016e13bfd73e2d
Arnol <andikamaster13@gmail.com>
2026-05-22 16:16:32 +0700

    update controll and model matakuliah

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
+use App\Http\Controllers\MataKuliahController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('matakuliah', MataKuliahController::class);
